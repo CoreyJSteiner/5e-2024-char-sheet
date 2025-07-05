7acfe41a850e89f1ec4d76d68ff145e1ab2de058
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-07-05 12:03:06 -0400

    updates nested helper to handle arrays and updates stae with weapons and damge changes

--- src/components/WeaponsAndDamage/WeaponsAndDamage.tsx
+++ src/components/WeaponsAndDamage/WeaponsAndDamage.tsx
@@ -1,5 +1,6 @@
 import { useCharSheetContext } from '../CharSheetContext'
 import InputHeading from '../InputHeading'
+import { updateNestedValue } from '../Utils/Helpers'
 import './WeaponsAndDamage.css'
 
 type WeaponsAndDamageProps = {
@@ -8,41 +9,48 @@ type WeaponsAndDamageProps = {
 }
 
 const WeaponsAndDamage: React.FC<WeaponsAndDamageProps> = ({ id, className }) => {
-    const { charSheet } = useCharSheetContext()
+    const { charSheet, updateCharSheet } = useCharSheetContext()
+
+    const updateDamageField = (field: string, idx: number) => (input: string) => {
+        const updated = updateNestedValue(charSheet, ['weaponsAndDamage', idx, field], input)
+        console.log(updated);
+
+        updateCharSheet(updated)
+    }
 
     return (
         <div id={id} className={`damage-section-container ${className}`}>
             <div className={`damage-section`}>
                 {charSheet.weaponsAndDamage && (
-                    charSheet.weaponsAndDamage.map(damageLine => (
-                        <div className='damage-line'>
+                    charSheet.weaponsAndDamage.map((damageLine, idx) => (
+                        <div key={idx} className='damage-line'>
                             <InputHeading
                                 className='damage-line-name'
                                 propTextValue={damageLine.name}
                                 inputMode='text'
                                 headingSize='h4'
-                                onUpdate={() => console.log('name')}
+                                onUpdate={updateDamageField('name', idx)}
                             />
                             <InputHeading
                                 className='damage-line-atk-dc'
                                 propTextValue={damageLine.atkBonusOrDc}
                                 inputMode='text'
                                 headingSize='h4'
-                                onUpdate={() => console.log('atkBonusOrDc')}
+                                onUpdate={updateDamageField('atkBonusOrDc', idx)}
                             />
                             <InputHeading
                                 className='damage-line-dmg-type'
                                 propTextValue={damageLine.damageAndType}
                                 inputMode='text'
                                 headingSize='h4'
-                                onUpdate={() => console.log('damageAndType')}
+                                onUpdate={updateDamageField('damageAndType', idx)}
                             />
                             <InputHeading
                                 className='damage-line-notes'
                                 propTextValue={damageLine.notes}
                                 inputMode='text'
                                 headingSize='h4'
-                                onUpdate={() => console.log('notes')}
+                                onUpdate={updateDamageField('notes', idx)}
                             />
                         </div>
                     ))
