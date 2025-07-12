43d276fd56a54e005a29cb8c7e8e81f7194db8c9
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-07-12 10:37:51 -0400

    map through feature attributes

--- src/components/FeatureEntry/FeatureEntry.tsx
+++ src/components/FeatureEntry/FeatureEntry.tsx
@@ -9,7 +9,23 @@ type FeatureEntryProps = {
 const FeatureEntry: React.FC<FeatureEntryProps> = ({ className, feature }) => {
     return (
         <div className={`feature-entry-container ${className}`}>
-            <p>{feature.body}</p>
+            {feature.maxUses && (
+                <p>{feature.maxUses}/{feature.currentUses || 0}</p>
+            )}
+            {feature.reset && (
+                <p>{feature.reset === 'LR' ? 'Long Rest' : 'Short Rest'}</p>
+            )}
+            {feature.title && (
+                <p>{feature.title}</p>
+            )}
+            {feature.heading && (
+                <p>{feature.heading}</p>
+            )}
+            {feature.body && (
+                <p>{feature.body}</p>
+            )}
+
+
         </div>
     )
 }
